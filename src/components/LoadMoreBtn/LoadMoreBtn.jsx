85f6da503be25ec0870eaf946a30473cd4e9f0c1
13DDC <150695013+13DDC@users.noreply.github.com>
2024-08-22 18:56:30 +0300

    hw 4

---
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoadMoreBtn = ({ loadMore }) => {
+  return (
+    <div>
+      <button onClick={loadMore}></button>
+    </div>
+  );
+};
+
+export default LoadMoreBtn;
